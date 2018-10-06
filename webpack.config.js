15cd75dc2d3e214b77fadcf28265189f1ea7728c
Ciao Chung <ciao0958@gmail.com>
2018-10-06 14:48:38 +0800

    加入index.html載入bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path')
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
 }
