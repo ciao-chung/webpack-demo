167e836a61273faf1951cf9ccbb47254abce2cc3
Ciao Chung <ciao0958@gmail.com>
2018-10-06 15:33:44 +0800

    使用mini-css-extract-plugin把style獨立出來

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import './index.sass'
 const name = 'ciao'
 const age = 25
-console.log(`${name}: ${age}`);
+console.log(`${name}: ${age}`)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
@@ -32,10 +33,16 @@ module.exports = {
         test: /\.sass$/,
         use: [
           'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'sass-loader',
         ],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
 }
